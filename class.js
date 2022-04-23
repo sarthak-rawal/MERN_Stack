20545dd334c98b197e45c367d39d83e070f5e8f0
Sarthak Rawal <93375570+sarthak-rawal@users.noreply.github.com>
2022-04-23 22:36:56 +0545

    Add files via upload

---
+++ class.js
@@ -0,0 +1,49 @@
+// var l = 22;
+// var peri = 4*l;
+// console.log(peri)
+// if (peri > 50 ){
+//     console.log('big rect')
+// }
+// else{
+//     console.log('small rect')
+// }
+// function multiplynum(num1,num2){
+
+//     console.log(num1* num2)
+// }
+// multiplynum(2,3)
+// function percentage(tm,om){
+//    per=(om/tm)*100
+//    console.log(per)
+//    if(per<=100 && per>90){
+//     console.log('excellent')
+// }
+// else if(per<=90 && per>80){
+//     console.log('very good')
+// }
+// else{
+//     console.log('good')
+// }
+// }
+// percentage(500,450)
+// function divisiblebyten(num1){
+//     if(num1%10 == 0){
+//         console.log(Boolean(true))
+//     }else{
+//         console.log(Boolean(false))
+//     }
+// }
+// divisiblebyten(33)
+// function ageoftwo(age1,age2){
+//     if(age1>age2){
+//         console.log('age1 is oldest')
+//     }else{
+//         console.log('age2 is oldest')
+//     }
+// // }
+// ageoftwo(111,22)
+function retiremnetat65(age){
+    ewt= 65-age
+    console.log('remaining years to work',ewt)
+}
+retiremnetat65(55)
